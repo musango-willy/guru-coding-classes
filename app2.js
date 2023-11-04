fc71a69ac3451c7e53d06ee0358f93d1298dd5f8
musango willy <musangowilly@gmail.com>
2023-11-04 10:10:57 +0300

    oook

--- app2.js
+++ app2.js
@@ -23,3 +23,39 @@ const wycliffe=new js("wycliffe","musango","willy",23,"musangowilly@gmail.com");
 const lewis=new js("lewis","machabe",37,"lewis@gmail.com");
 
 const jsArr=[sharon,lewis]
+
+
+//new object constructors and prototype
+
+//object constructors 
+
+function js(firstname,lastname,age,email){
+
+    this.firstname=firstname;
+    this.lastname=lastname;
+    this.age=age;
+    this.email=email;
+}
+
+js.prototype.getFullname=function(){
+    return this.firstname + " " + this.lastname;
+}
+const name="sharon";
+
+function call(){
+    console.log(name)
+
+}
+call();
+
+const sharon=new js("sharon","nyamogo",19,"sharon@gmail.com");
+const lewis=new js("lewis","machabe",27,"lewis@gmail.com");
+
+console.log(sharon);
+console.log(lewis);
+
+const jsArr=[sharon,lewis]
+
+console.log(jsArr);
+
+//object prototypes
