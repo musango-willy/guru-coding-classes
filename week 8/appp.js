c483f5b40c5763a67b555002d83b06c0f78bed87
musango willy <musangowilly@gmail.com>
2024-02-19 22:16:01 +0300

    yesas

--- week 8/appp.js
+++ week 8/appp.js
@@ -6,3 +6,9 @@ function obj(name,age,email){
     this.email =email;
 }
 const person = new obj("sharon",25,"sharon@gmail.com");
+
+Number('20')
+
+ function fetchData(call){
+    setTimeout(function(){call("data fetched")},5000)
+ }
